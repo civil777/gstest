6413410ba431cf3412cf8007acc3b9549566ecd7
Ubuntu <ubuntu@ubuntu.skbroadband>
2021-01-09 14:14:54 +0900

    1.0

---
+++ config/urls.py
@@ -0,0 +1,34 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("", include("core.urls", namespace="core")),
+    path("products/", include("rooms.urls", namespace="rooms")),
+    path("admin/", admin.site.urls),
+    path("users/", include("users.urls", namespace="users")),
+]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# 나중에 아마존에 추가한다면 if settings.DEBUG: 를 위에 urlpatterns의 윗줄에 추가하고 urlpatterns는 들여쓰기 한번하기
+
+
